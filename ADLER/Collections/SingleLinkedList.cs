e0928b00d44722474b9b067a48be96f80e5b2fb4
Maikel Hofman <mh.carducci@gmail.com>
2013-11-09 03:03:09 +0100

    Added basic skeleton for SingleLinkedList

---
+++ ADLER/Collections/SingleLinkedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLER.Collections
+{
+    public class SingleLinkedList<T> : IList<T>
+    {
+        private SingleLinkedNode<T> _startNode;
+        private int _size;
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            // Check of root Node !=  null
+            // if null insert as root node
+            if (this._startNode == null)
+            {
+                this._startNode = new SingleLinkedNode<T>(item);
+            }
+            else
+            {
+                SingleLinkedNode<T> temp = _startNode;
+
+                while (temp.Next == null)
+                {
+                    temp = temp.Next;
+                }
+
+                // End of list found insert element 
+                temp.Next = new SingleLinkedNode<T>(item);
+            }
+
+            this._size++;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { return _size; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ADLER/Collections/SingleLinkedNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLER.Collections
+{
+    internal class SingleLinkedNode<T>
+    {
+        private T _value;
+        private SingleLinkedNode<T> _next;
+
+        public SingleLinkedNode() : this(default(T), null) 
+        {
+            
+        }
+
+        public SingleLinkedNode(T Value) : this(Value, null)
+        {
+            
+        }
+
+        public SingleLinkedNode(T Value, SingleLinkedNode<T> Next)
+        {
+            this._value = Value;
+            this._next = Next;
+        }
+
+        public T Value
+        {
+            get { return this._value; }
+            set { this._value = value;}
+        }
+
+        public SingleLinkedNode<T> Next 
+        {
+            get{ return this._next; }
+            set { this._next = value; }
+        }
+
+    }
+}
