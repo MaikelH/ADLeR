636f2ddf51b180980ce802633abe66ba5618a210
Maikel Hofman <mh.carducci@gmail.com>
2013-11-10 03:24:36 +0100

    Added SingleLinkedList tests and removed some erros in Remove()

--- ADLER/Collections/SingleLinkedList.cs
+++ ADLER/Collections/SingleLinkedList.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ADLER.Collections
 {
@@ -31,11 +28,18 @@ namespace ADLER.Collections
         {
             get
             {
-                throw new NotImplementedException();
+                SingleLinkedNode<T> temp = _startNode;
+                
+                for (int i = 0; i < index; i++)
+                {
+                    temp = _startNode.Next;
+                }
+
+                return temp.Value;
             }
             set
             {
-                throw new NotImplementedException();
+                Insert(0, value);
             }
         }
 
@@ -45,19 +49,20 @@ namespace ADLER.Collections
         /// <param name="item">Item of type T to insert</param>
         public void Add(T item)
         {
-            if(!this._readOnly)
+            if(!_readOnly)
             {
                 // Check of root Node !=  null
                 // if null insert as root node
-                if (this._startNode == null)
+                if (_startNode == null)
                 {
-                    this._startNode = new SingleLinkedNode<T>(item);
+                    _startNode = new SingleLinkedNode<T>(item);
                 }
                 else
                 {
                     SingleLinkedNode<T> temp = _startNode;
 
-                    while (temp.Next == null)
+                    // ReSharper disable once PossibleNullReferenceException
+                    while (temp.Next != null)
                     {
                         temp = temp.Next;
                     }
@@ -66,7 +71,7 @@ namespace ADLER.Collections
                     temp.Next = new SingleLinkedNode<T>(item);
                 }
 
-                this._size++;
+                _size++;
             }
         }
 
@@ -75,16 +80,21 @@ namespace ADLER.Collections
         /// </summary>
         public void Clear()
         {
-            if (!this._readOnly)
+            if (!_readOnly)
             {
-                this._startNode = null;
-                this._size = 0;
+                _startNode = null;
+                _size = 0;
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public bool Contains(T item)
         {
-            SingleLinkedNode<T> temp = this._startNode;
+            SingleLinkedNode<T> temp = _startNode;
 
             while (temp.Next != null)
             {
@@ -118,7 +128,7 @@ namespace ADLER.Collections
         /// </summary>
         public bool IsReadOnly
         {
-            get { return this._readOnly; }
+            get { return _readOnly; }
         }
 
         /// <summary>
@@ -128,27 +138,38 @@ namespace ADLER.Collections
         /// <returns>True if element is removed</returns>
         public bool Remove(T item)
         {
-            if (!_readOnly)
-            {
-                SingleLinkedNode<T> temp = this._startNode;
 
-                if (temp.Equals(item))
+            bool returnVal = false;
+            if (!_readOnly)
+            { 
+                if (_startNode.Value.Equals(item))
                 {
-                    return removeAfter(temp);
+                    _startNode = _startNode.Next;
+                    returnVal = true;
                 }
                 else
                 {
+                    SingleLinkedNode<T> temp = _startNode;
                     while (temp.Next != null)
                     {
-                        if (temp.Next.Equals(item))
+                        if (temp.Next.Value.Equals(item))
                         {
-                            return removeAfter(temp);
+                            returnVal = removeAfter(temp);
+                            // Break is used here because there should be only one place for return.
+                            // This because the size decrement can always be guaranteed
+                            break;
                         }
-                    }
+
+                        temp = temp.Next;
+                    }    
                 }
             }
 
-            return false;
+            if (returnVal)
+            {
+                _size--;
+            }
+            return returnVal;
         }
 
         public IEnumerator<T> GetEnumerator()
---
+++ ADLeR.Test/SingleLinkedListTest.cs
@@ -0,0 +1,65 @@
+﻿using ADLER.Collections;
+using NUnit.Framework;
+
+namespace ADLeR.Test
+{
+    [TestFixture]
+    class SingleLinkedListTest
+    {
+
+        [Test]
+        public void TestAdd()
+        {
+            SingleLinkedList<int> testList = new SingleLinkedList<int>();
+            testList.Add(0);
+            testList.Add(1);
+
+            Assert.AreEqual(0, testList[0]);
+            Assert.AreEqual(1, testList[1]);
+            Assert.AreEqual(2, testList.Count);
+        }
+
+        [Test]
+        public void TestRemoveStart()
+        {
+            SingleLinkedList<int> testList = new SingleLinkedList<int>();
+            testList.Add(0);
+            testList.Add(1);
+
+            testList.Remove(0);
+
+            Assert.AreEqual(1, testList[0]);
+            Assert.AreEqual(1, testList.Count);
+        }
+
+        [Test]
+        public void TestRemoveMiddle()
+        {
+            SingleLinkedList<int> testList = new SingleLinkedList<int>();
+            testList.Add(0);
+            testList.Add(1);
+            testList.Add(2);
+
+            testList.Remove(1);
+
+            Assert.AreEqual(0, testList[0]);
+            Assert.AreEqual(2, testList[2]);
+            Assert.AreEqual(2, testList.Count);
+        }
+
+        [Test]
+        public void TestRemoveEnd()
+        {
+            SingleLinkedList<int> testList = new SingleLinkedList<int>();
+            testList.Add(0);
+            testList.Add(1);
+            testList.Add(2);
+
+            testList.Remove(2);
+
+            Assert.AreEqual(0, testList[0]);
+            Assert.AreEqual(1, testList[2]);
+            Assert.AreEqual(2, testList.Count);
+        }
+    }
+}
