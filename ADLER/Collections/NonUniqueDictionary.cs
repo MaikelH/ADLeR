8d5d3d603623e14a4b5f580ed705ad6d5b75b64b
M. Hofman <m.hofman@chronos-technology.nl>
2015-01-29 22:37:02 +0100

    First version of NonUniqueDictionary

---
+++ ADLER/Collections/NonUniqueDictionary.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewsRetriever
+{
+    public class NonUniqueDictionary<T, X>
+    {
+        private Dictionary<T, HashSet<X>> _internalDictionary;
+
+        public NonUniqueDictionary()
+            : base()
+        {
+            _internalDictionary = new Dictionary<T, HashSet<X>>();
+        }
+
+
+        public IEnumerator<KeyValuePair<T, HashSet<X>>> GetEnumerator()
+        {
+            return _internalDictionary.GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<T, HashSet<X>> item)
+        {
+            if (_internalDictionary.ContainsKey(item.Key))
+            {
+                _internalDictionary[item.Key].UnionWith(item.Value);
+            }
+            else
+            {
+                _internalDictionary.Add(item.Key, item.Value);
+            }
+        }
+
+        public void Clear()
+        {
+            _internalDictionary.Clear();
+        }
+
+        public bool Contains(X obj)
+        {
+            return _internalDictionary.Keys.ToList()
+                .Select(x => _internalDictionary[x]
+                .Contains(obj))
+                .Aggregate((b, b1) => b || b1);
+        }
+
+        public bool Contains(KeyValuePair<T, HashSet<X>> item)
+        {
+            return _internalDictionary.Contains(item);
+        }
+
+
+        public bool Remove(T key, X item)
+        {
+            return _internalDictionary[key].Remove(item);
+        }
+
+        public bool Remove(T key)
+        {
+            return _internalDictionary.Remove(key);
+        }
+
+        public int Count
+        {
+            get { return _internalDictionary.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool ContainsKey(T key)
+        {
+            return _internalDictionary.ContainsKey(key);
+        }
+
+        public void Add(T key, X value)
+        {
+            if (_internalDictionary.ContainsKey(key))
+            {
+                _internalDictionary[key].Add(value);
+            }
+            else
+            {
+                _internalDictionary.Add(key, new HashSet<X> { value });
+            }
+        }
+
+        public void Add(T key, IEnumerable<X> value)
+        {
+            _internalDictionary.Add(key, new HashSet<X>(value));
+        }
+
+        public bool TryGetValue(T key, out HashSet<X> value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<X> this[T key]
+        {
+            get { return _internalDictionary[key]; }
+            set { _internalDictionary[key] = value; }
+        }
+
+        public ICollection<T> Keys
+        {
+            get { return _internalDictionary.Keys; }
+        }
+
+        public ICollection<HashSet<X>> Values
+        {
+            get
+            {
+                List<HashSet<X>> values = new List<HashSet<X>>();
+
+                _internalDictionary.Keys.ToList().ForEach(x => values.Add(_internalDictionary[x]));
+
+                return values;
+            }
+        }
+    }
+}
---
+++ ADLeR.Test/UniqueDictionaryTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using NewsRetriever;
+using NUnit.Framework;
+
+namespace ADLeR.Test
+{
+
+    [TestFixture]
+    public class UniqueDictionaryTest
+    {
+        [Test]
+        public void InitTest()
+        {
+            NonUniqueDictionary<string, string> collection = new NonUniqueDictionary<string, string>();
+
+            Assert.IsNotNull(collection);
+        }
+
+        [Test]
+        public void IndexTest()
+        {
+            NonUniqueDictionary<string, string> collection = new NonUniqueDictionary<string, string>();
+            HashSet<string> set = new HashSet<string>{"1"};
+
+            collection.Add(new KeyValuePair<string, HashSet<string>>("0", set));
+
+            Assert.AreEqual(set, collection["0"]);
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            NonUniqueDictionary<string, string> collection = new NonUniqueDictionary<string, string>();
+            collection.Add("1", "2");
+
+            Assert.IsTrue(collection["1"].Contains("2"));
+        }
+
+        [Test]
+        public void CountTest()
+        {
+            NonUniqueDictionary<string, string> collection = new NonUniqueDictionary<string, string>();
+            collection.Add("1", "2");
+    
+            Assert.AreEqual(1, collection.Count);
+        }
+    }
+}
